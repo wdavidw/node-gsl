148b200f38049658f6b3380921811c27f9e45c5a
David Worms <david@adaltas.com>
2011-04-11 03:39:53 +0200

    Rename sample density to gaussian_density

--- samples/gaussian_density.js
+++ samples/gaussian_density.js
@@ -3,7 +3,7 @@ var random = require('gsl').random,
 	fs = require('fs'),
 	spawn = require('child_process').spawn;
 	
-	var stream = fs.createWriteStream(__dirname+'/density/function_seed_no.data');
+	var stream = fs.createWriteStream(__dirname+'/gaussian_density/function_seed_no.data');
 	var buf = '';
 	for(var i=0;i<10000;i++){
 		buf += random.gaussian();
@@ -12,7 +12,7 @@ var random = require('gsl').random,
 	stream.write(buf);
 	stream.end();
 	
-	var stream = fs.createWriteStream(__dirname+'/density/function_seed_10.data');
+	var stream = fs.createWriteStream(__dirname+'/gaussian_density/function_seed_10.data');
 	var buf = '';
 	for(var i=0;i<10000;i++){
 		buf += random.gaussian(10);
@@ -21,7 +21,7 @@ var random = require('gsl').random,
 	stream.write(buf);
 	stream.end();
 	
-	var stream = fs.createWriteStream(__dirname+'/density/object_seed_no.data');
+	var stream = fs.createWriteStream(__dirname+'/gaussian_density/object_seed_no.data');
 	var ran = new random.Gaussian();
 	var buf = '';
 	for(var i=0;i<10000;i++){
@@ -31,7 +31,7 @@ var random = require('gsl').random,
 	stream.write(buf);
 	stream.end();
 	
-	var stream = fs.createWriteStream(__dirname+'/density/object_seed_10.data');
+	var stream = fs.createWriteStream(__dirname+'/gaussian_density/object_seed_10.data');
 	var ran = new random.Gaussian();
 	var buf = '';
 	for(var i=0;i<10000;i++){
@@ -43,19 +43,19 @@ var random = require('gsl').random,
 	
 	var cmd = [''];
 	['function_seed_no','function_seed_10','object_seed_no','object_seed_10'].forEach(function(type){
-		cmd.push('png(filename="'+__dirname+'/density/'+type+'.png", height=295, width=500, bg="white")');
-		cmd.push('data <- read.csv("'+__dirname+'/density/'+type+'.data", header=F, sep="\\t")');
+		cmd.push('png(filename="'+__dirname+'/gaussian_density/'+type+'.png", height=295, width=500, bg="white")');
+		cmd.push('data <- read.csv("'+__dirname+'/gaussian_density/'+type+'.data", header=F, sep="\\t")');
 		cmd.push('d <- density(data[,1])');
 		cmd.push('plot(d)');
 		cmd.push('dev.off()');
 		cmd.push('');
 	});
 	
-	var stream = fs.createWriteStream(__dirname+'/density.r');
+	var stream = fs.createWriteStream(__dirname+'/gaussian_density.r');
 	stream.write(cmd.join('\n'));
 	stream.end();
 	stream.on('close', function(){
-		var r = spawn('R',['-q','-f',__dirname+'/density.r']);
+		var r = spawn('R',['-q','-f',__dirname+'/gaussian_density.r']);
 		r.on('exit', function(code){
 			if (code !== 0) {
 			    return console.log('Make sure R is installed');
